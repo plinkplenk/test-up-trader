f61e8eadcaf03f1d83dd67513016da34cc55fa38
plinkplenk <sergeyyudenichev@gmail.com>
2024-07-26 17:24:57 +0700

    feat: add header

--- menu/urls.py
+++ menu/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import MenusView, MenuView
 
 urlpatterns = [
-    path("", MenusView.as_view(), name="home"),
+    path("", MenusView.as_view(), name="index"),
     path("<str:name>/", MenuView.as_view()),
 ]
