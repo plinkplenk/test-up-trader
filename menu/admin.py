71d25f5ef5262022c5b73fdd4c32a690301c05b5
plinkplenk <sergeyyudenichev@gmail.com>
2024-07-26 17:20:05 +0700

    feat: update admin

--- menu/admin.py
+++ menu/admin.py
@@ -7,10 +7,14 @@ class MenuItemInline(admin.TabularInline):
     model = Menu
     fk_name = "parent"
     extra = 1
+    show_change_link = True
 
 
 class MenuAdmin(admin.ModelAdmin):
     inlines = [MenuItemInline]
+    list_filter = ("parent",)
+    ordering = ("name", "id")
+    list_display = ("id", "name")
 
 
 admin.site.register(Menu, MenuAdmin)
